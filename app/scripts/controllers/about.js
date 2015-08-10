dd1faa269bb1c2455cc6976fc7b3b0282b7545d3
Matthew R <matthew@mjrouser.com>
2015-08-10 12:01:37 -0400

    initial scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eeaDoerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eeaDoerApp
+ */
+angular.module('eeaDoerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eeaDoerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eeaDoerApp
+ */
+angular.module('eeaDoerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
