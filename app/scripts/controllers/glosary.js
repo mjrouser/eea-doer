50ad75e479d02b12ecae53594285ed25ac528ec3
Matthew R <matthew@mjrouser.com>
2015-08-17 15:38:35 -0400

    added logo, switched controlers

---
+++ app/scripts/controllers/glosary.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eeaDoerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eeaDoerApp
+ */
+angular.module('eeaDoerApp')
+  .controller('GlosCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
